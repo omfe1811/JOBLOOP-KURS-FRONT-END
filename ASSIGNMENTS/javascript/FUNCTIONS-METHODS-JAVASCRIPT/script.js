bfafcc98863ae0d818fe93fc26acfc527474e901
Olga <omfe1811@gmail.com>
2025-05-10 14:45:06 +0200

    js styling

---
+++ ASSIGNMENTS/javascript/FUNCTIONS-METHODS-JAVASCRIPT/script.js
@@ -0,0 +1,13 @@
+
+function
+✅ Task 1
+Create the following function:
+The function should take a number as a parameter and return:
+"Odd number" if the number is odd
+"Even number" if the number is even
+(Use return, don't use console.log inside the function)
+
+evenOrOdd(number){
+    
+    return isEven;
+}
