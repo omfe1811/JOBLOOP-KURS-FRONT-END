65684dac79f58e7e5255bdd94abd020abfffe2b3
Olga <omfe1811@gmail.com>
2025-05-12 23:19:23 +0200

    methods

--- ASSIGNMENTS/javascript/FUNCTIONS-METHODS-JAVASCRIPT/script.js
+++ ASSIGNMENTS/javascript/FUNCTIONS-METHODS-JAVASCRIPT/script.js
@@ -1,13 +1,18 @@
-
-function
-✅ Task 1
-Create the following function:
-The function should take a number as a parameter and return:
-"Odd number" if the number is odd
-"Even number" if the number is even
-(Use return, don't use console.log inside the function)
-
-evenOrOdd(number){
-    
-    return isEven;
+/* 
+The function should return:
+"Invalid time" if the hour is less than 0
+"Good night (name)" if hour is between 0–5
+"Good morning (name)" if hour is between 6–11
+"Good day (name)" if hour is between 12–17
+"Good evening (name)" if hour is between 18–23
+"Invalid time" if hour is greater than 23
+If no hour is received, return an error message */
+function welcome(name, hour){
+    if (hour<0&&hour>=23){
+        message="Invalid time"
+    }else if(hour=null){
+        message="Error! Type in hour"
+    }else if (hour<=5){
+        message="Good night "
+    }
 }
